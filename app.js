48d771bab191ccad609cb6f0985297d344fdc920
anouaraissani <aaissani571@gmail.com>
2023-03-07 20:26:47 +0100

    add 6-alternative-flavor file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const names = require('./4-names.js')
 const sayHi = require('./5-utils.js')
-
+const data = require('./6-alternative-flavor.js')
 sayHi(names.john)
 sayHi(names.peter)
+console.log(data)
