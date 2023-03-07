c07753eea4864609c18508c7323af6ebdd3d77fe
anouaraissani <aaissani571@gmail.com>
2023-03-07 20:56:21 +0100

    add file modules.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const names = require('./4-names')
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')
-sayHi(names.john)
-sayHi(names.peter)
-console.log(data)
-
-require('./7-mind-grenade')
