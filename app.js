d8c92b652f6bf60b7ab1bc222f07b8bb8398d91d
anouaraissani <aaissani571@gmail.com>
2023-03-07 18:39:23 +0100

    add names examples with sayHi function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-setInterval(()=>{console.log('Hello World!')},1000)
+const secret = "SUPER SECRET"
+const john = "john"
+const peter = "peter"
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi(secret)
+sayHi(john)
+sayHi(peter)
