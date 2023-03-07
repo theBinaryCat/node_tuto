aa14f4182b718dcd24ac1d726369186e56dd616b
anouaraissani <aaissani571@gmail.com>
2023-03-07 20:54:26 +0100

    add file 7-min-grenade.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const names = require('./4-names.js')
-const sayHi = require('./5-utils.js')
-const data = require('./6-alternative-flavor.js')
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
 sayHi(names.john)
 sayHi(names.peter)
 console.log(data)
+
+require('./7-mind-grenade')
