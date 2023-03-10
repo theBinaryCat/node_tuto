bcf05a43308d9b872af91eb2b2e8bf050c624f9e
theBinaryCat <noua.san@gmail.com>
2023-03-10 21:21:37 +0100

    add file read-file.js

--- 1-event-loop-examples/1-read-file.js
+++ 1-event-loop-examples/1-read-file.js
@@ -1,7 +1,7 @@
 const{readFile, writeFile} = require('fs')
 
 console.log('started a first task')
-readFile('./content/first.txt','utf8', (err, result)=>{
+readFile('../content/first.txt','utf8', (err, result)=>{
     if(err){
         console.log(err)
         return
