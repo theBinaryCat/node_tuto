5677bf71662258a988c207a2dacf5d98218388a1
theBinaryCat <noua.san@gmail.com>
2023-03-11 14:50:32 +0100

    add readFilePromise and writeFilePromise functions

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-const {readFile} = require('fs')
+const {readFile, writeFile} = require('fs')
+const util = require('util')
 
-const getText = (path) => {
-    return new Promise((resolve, reject) => {
-        readFile(path,'utf8', (err, data)=>{
-            if(err){
-                reject(err)
-            }else{
-                resolve(data)
-            }
-        })
-    })
-}
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
 
 
 const start = async () => {
     try{
-        const first = await getText('./content/first.txt')
-        const second = await getText('./content/second.txt')
+        const first = await readFilePromise('./content/first.txt','utf8')
+        const second = await readFilePromise('./content/second.txt','utf8')
+        await writeFilePromise(
+            './content/result-mind-grenade.txt',
+            `this is awsome ${first} ${second}`
+            )
         console.log(first, second)
     }catch(error){
         console.log(error)
@@ -24,6 +20,20 @@ const start = async () => {
     }
 }
 start()
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path,'utf8', (err, data)=>{
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+
 //getText('./content/first.txt')
 //.then ((result) => console.log(result))
 //.catch((err)=>console.log(err))
