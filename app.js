a2b5055e2770182b6ed00dd077bec50d1da38d67
theBinaryCat <noua.san@gmail.com>
2023-03-11 14:51:31 +0100

    use promises with fs module

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const {readFile, writeFile} = require('fs')
-const util = require('util')
+const {readFile, writeFile} = require('fs').promises
 
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile)
 
 
 const start = async () => {
     try{
-        const first = await readFilePromise('./content/first.txt','utf8')
-        const second = await readFilePromise('./content/second.txt','utf8')
-        await writeFilePromise(
+        const first = await readFile('./content/first.txt','utf8')
+        const second = await readFile('./content/second.txt','utf8')
+        await writeFile(
             './content/result-mind-grenade.txt',
             `this is awsome ${first} ${second}`
             )
