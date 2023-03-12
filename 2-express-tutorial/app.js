43518cfd309ed05a644338bc80a206f58aa70073
theBinaryCat <noua.san@gmail.com>
2023-03-12 22:09:21 +0100

    add url pages

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,8 +1,33 @@
 const http = require('http')
 
 const server = http.createServer((req, res) =>{
-    console.log('user hit the server')
-    res.end('home page')
+    console.log(req.method) //type the req method: (GET)
+    const url = req.url  
+
+    //home page
+    if(url === '/')
+    {
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>Home Page</h1>')
+        res.end()
+    }
+
+    //about page
+    else if(url ==='/about')
+    {
+        res.writeHead(200,{'content-type':'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    }
+
+    //page npt found
+    else 
+    {
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>Page Not Found</h1>')
+        res.end()
+    }
+    
 })
 
 server.listen(5000)
