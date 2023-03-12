1eebc80220a4c2f4086ffa3c32f2bd75c5562615
theBinaryCat <noua.san@gmail.com>
2023-03-12 15:59:32 +0100

    add file 16-streams.js

---
+++ 16-streams.js
@@ -0,0 +1,10 @@
+const {createReadStream, createWriteStream} = require('fs')
+
+const readStream = createReadStream('./content/big.txt')
+
+const writeStream = createWriteStream('./content/writeBigFile.txt')
+
+readStream.on('data', (chunk) => {
+    console.log('new chunk received')
+    writeStream.write(chunk)
+})
