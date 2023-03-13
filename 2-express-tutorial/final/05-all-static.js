a30b7da0a8c7ccc435203af26805e2433b0fe433
theBinaryCat <noua.san@gmail.com>
2023-03-13 16:34:54 +0100

    move code to 05-all-static.js

--- 2-express-tutorial/final/05-all-static.js
+++ 2-express-tutorial/final/05-all-static.js
@@ -1,21 +1,18 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 
-const app = express()
-
-// setup static and middleware
+//setup static and middleware
 app.use(express.static('./public'))
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-//   adding to static assets
-//   SSR
-// })
+//app.get('/', (req, res) => {
+//    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+//})
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+    res.status(404).send('Page Not Found')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
+    console.log('Server listening on port 5000...')
 })
