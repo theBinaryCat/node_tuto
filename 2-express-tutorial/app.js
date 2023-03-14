f7dfaddd479917d1b4c74773fa2fba781edb99a0
theBinaryCat <noua.san@gmail.com>
2023-03-14 16:18:30 +0100

    get product with id = 1

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -7,12 +7,18 @@ app.get('/', (req, res) => {
     res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
 })
 
+app.get('/api/products/1', (req, res) => {
+    const singleProduct = products.find(product => product.id === 1)
+    console.log(singleProduct)
+    res.json(singleProduct)
+})
+
 app.get('/api/products', (req, res) => {
     const newProducts = products.map((product) => {
         const {id, name, image} = product
         return {id, name, image}
     })
-    res.send(newProducts)
+    res.json(newProducts)
 })
 
 app.listen((5000),() => {
