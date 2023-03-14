93203d7f44bd02034cfcf56d7c62b102b27d678f
theBinaryCat <noua.san@gmail.com>
2023-03-14 15:20:08 +0100

    basic json

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,18 +1,13 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 
-//setup static and middleware
-app.use(express.static('./public'))
+const {products} = require('./data.js')
 
-//app.get('/', (req, res) => {
-//    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-//})
-
-app.all('*', (req, res) => {
-    res.status(404).send('Page Not Found')
+app.get('/', (req, res) => {
+    res.json(products)
 })
 
-app.listen(5000, () => {
-    console.log('Server listening on port 5000...')
+
+app.listen((5000),() => {
+    console.log('Server is listening on port 5000...')
 })
--- 2-express-tutorial/final/06-basic-json.js
+++ 2-express-tutorial/final/06-basic-json.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-const { products } = require('./data')
+
+const {products} = require('./data.js')
+
 app.get('/', (req, res) => {
-  res.json(products)
+    res.json(products)
 })
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+
+app.listen((5000),() => {
+    console.log('Server is listening on port 5000...')
 })
