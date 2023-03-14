e6b028abcbd5776b2d52b7a7fa78ef327c4ea55f
theBinaryCat <noua.san@gmail.com>
2023-03-14 16:05:50 +0100

    get id, name, image

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -4,9 +4,16 @@ const app = express()
 const {products} = require('./data.js')
 
 app.get('/', (req, res) => {
-    res.json(products)
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
 })
 
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.send(newProducts)
+})
 
 app.listen((5000),() => {
     console.log('Server is listening on port 5000...')
