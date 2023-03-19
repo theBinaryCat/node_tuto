c2f3b717b9b606d30b0243d5374e4680432acfa3
theBinaryCat <noua.san@gmail.com>
2023-03-19 18:14:50 +0100

    add route does not exist message

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,10 +4,12 @@ const connectDB = require('./db/connect')
 require('dotenv').config()
 const port = 3000
 const tasks = require('./routes/tasks.js')
+const notFound = require('./middleware/not-Found.js')
 
 app.use(express.static('./public'))
 app.use(express.json())
 app.use('/api/v1/tasks',tasks)
+app.use(notFound)
 
 const start = async() => {
     try{
